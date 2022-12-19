0f23b2817eb050eea6cbb6aee7ab39473c2e9f47
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2022-12-19 19:21:54 +0100

    transfered all forecast data in one component

--- src/ForecastDate.js
+++ src/ForecastDate.js
@@ -1,21 +1,38 @@
 import React from "react";
 
-export default function ForecastDate (props){
+export default function ForecastDate(props) {
+  function day() {
+    let date = new Date(props.date.time * 1000);
+    let day = date.getDay();
 
-    let days = [
-      "Sun",
-      "Mon",
-      "Tue",
-      "Wed",
-      "Thu",
-      "Fri",
-      "Sat",
-    ];
-    let day = days[props.date.getDay()];
-   
-    return (
-      <div>
-        {day} 
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+
+  return (
+    <div className="forecast mt-4">
+      <div className="row">
+        <div className="col">
+          <div className="forecast-day">
+            {day()}
+          </div>
+          <div>
+            {" "}
+            <img
+              src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${props.date.condition.icon}.png`}
+              alt=""
+            />
+          </div>
+          <span className="max-temp">
+            {Math.round(props.date.temperature.maximum)}°C
+          </span>
+          <span className="min-temp">
+            {Math.round(props.date.temperature.minimum)}°F
+          </span>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -8,35 +8,13 @@ export default function WeatherForecast(props) {
   const [forecastData, setForecastData] = useState(null);
 
   function displayForecast(response) {
-  
     setForecastData(response.data.daily);
     setloaded(true);
   }
 
   if (loaded) {
-    return (
-      <div className="forecast mt-4">
-        <div className="row">
-          <div className="col">
-            <div className="forecast-day">
-              <ForecastDate date={new Date(forecastData[0].time)} />
-            </div>
-            <div>
-              {" "}
-              <img
-                src={`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastData[0].condition.icon}.png`} alt=""
-              />
-            </div>
-            <span className="max-temp">
-              {Math.round(forecastData[0].temperature.maximum)}°C
-            </span>
-            <span className="min-temp">
-              {Math.round(forecastData[0].temperature.minimum)}°F
-            </span>
-          </div>
-        </div>
-      </div>
-    );
+    
+    return <ForecastDate date={forecastData[0]} />;
   } else {
     let apiKey = "ee4b35717d055b166do409ddtcf1532a";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}`;
